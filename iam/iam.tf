956831b13e6427131c40cc304b4761ae2ba9c63f
sai <sainath907@gmail.com>
2022-04-12 12:36:49 +0530

    adding iam user and volume

--- iam/iam.tf
+++ iam/iam.tf
@@ -53,3 +53,24 @@ resource "aws_iam_policy_attachment" "test-attach" {
   groups     = [aws_iam_group.group.name]
   policy_arn = aws_iam_policy.policy.arn
 }
+
+resource "aws_volume_attachment" "ebs_att" {
+  device_name = "/dev/sdh"
+  volume_id   = aws_ebs_volume.example.id
+  instance_id = aws_instance.web.id
+}
+
+resource "aws_instance" "web" {
+  ami               = "ami-00ee4df451840fa9d"
+  availability_zone = "us-west-2a"
+  instance_type     = "t2.micro"
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
+
+resource "aws_ebs_volume" "example" {
+  availability_zone = "us-west-2a"
+  size              = 1
+}
